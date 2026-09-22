00da033bc252db7cbd80c5629456c835036ced6f
fatimakamranh-sys <fatimakamranh@gmail.com>
2026-09-22 17:14:20 +0500

    Added calculator with basic operations

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+num = float(input("enter first number:"))
+operation = input("enter operation (+, -, *, /): ")
+num2 = float(input("enter second number:"))
+
+if operation == "+":
+    result = num + num2
+elif operation == "-":
+    result= num - num2
+elif operation == "*":
+    result = num * num2
+elif operation == "/":
+    result = num/num2
+else: 
+    print("invalid operation")
+    result = None
+
+if result is not None:
+    print(f"{num} { operation} {num2} = { result}")
