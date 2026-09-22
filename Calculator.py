3ef116e225b2091ba017f8c441df72e724a42da9
fatimakamranh-sys <fatimakamranh@gmail.com>
2026-09-22 17:34:24 +0500

    Added loop

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,25 @@
-num = float(input("enter first number:"))
-operation = input("enter operation (+, -, *, /): ")
-num2 = float(input("enter second number:"))
+while True:
+   num = float(input("enter first number:"))
+   operation = input("enter operation (+, -, *, /): ")
+   num2 = float(input("enter second number:"))
 
-if operation == "+":
-    result = num + num2
-elif operation == "-":
-    result= num - num2
-elif operation == "*":
-    result = num * num2
-elif operation == "/":
-    result = num/num2
-else: 
-    print("invalid operation")
-    result = None
+   if operation == "+":
+     result = num + num2
+   elif operation == "-":
+     result= num - num2
+   elif operation == "*":
+     result = num * num2
+   elif operation == "/":
+     result = num/num2
+   else: 
+     print("invalid operation")
+     result = None
 
-if result is not None:
-    print(f"{num} { operation} {num2} = { result}")
+   if result is not None:
+     print(f"{num} { operation} {num2} = { result}")
+
+   again = input("calculate again? (y/n):")
+   if again.lower() != "y":
+     print("Goodbye")
+     break
+    
