971ba831e7cbf217ca622d998631b7cc86d6b49d
fatimakamranh-sys <fatimakamranh@gmail.com>
2026-09-26 13:23:30 +0500

    add categories: images, docs, videos, audio, misc

--- File organizer.py
+++ File organizer.py
@@ -1,6 +1,13 @@
 import os
 import shutil
 
+categories = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif"],
+    "Docs": [".pdf", ".docx", ".txt"],
+    "Videos": [".mp4", ".mkv", ".avi"],
+    "Audio": [".mp3", ".wav"],
+}
+
 path=input("enter folder path:"). strip()
 
 for file in os.listdir(path):
@@ -12,7 +19,13 @@ for file in os.listdir(path):
 
     if not ext:
         continue
-    folder_path = os.path.join(path, ext.upper() + "_Files")
+    folder_name = "misc_files"
+    for category, extensions in categories.items():
+        if"." + ext in extensions:
+            folder_name = category + "_files"
+            break
+
+    folder_path = os.path.join(path, folder_name)
     os.makedirs(folder_path, exist_ok=True)
 
     
